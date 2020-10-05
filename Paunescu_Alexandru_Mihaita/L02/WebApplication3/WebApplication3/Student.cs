4cfd07ef537e4cb91c41a101ec38e6678470149d
paunescumihaita <62847022+paunescumihaita@users.noreply.github.com>
2020-10-05 17:36:21 +0300

    LAB 2

---
+++ Paunescu_Alexandru_Mihaita/L02/WebApplication3/WebApplication3/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string nume { get; set; }
+        public string prenume { get; set; }
+        public int an { get; set; }
+        public string facultate { get; set; }
+        public float mediegenerala { get; set; }
+    
+    }
+}
---
+++ Paunescu_Alexandru_Mihaita/L02/WebApplication3/WebApplication3/StudentRepo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public static class StudentRepo
+    {
+        public static List<Student> st = new List<Student>();
+       
+        public static void adaugare(Student s)
+        {
+            st.Add(s);
+        }
+      
+        
+        
+        
+
+        public static Student cautare(int id)
+        {
+            foreach(Student s in st)
+            {
+                if (s.id == id)
+                    return s;
+            }
+            return null;
+        }
+        public static void stergere(int id)
+        {
+            int i;
+            foreach (Student s in st)
+            {
+                if (s.id == id)
+                {
+                    i = st.IndexOf(s);
+                    st.RemoveAt(i);
+                }
+                    
+            }
+
+        }
+        public static List<Student> afisare()
+        {
+            return st;
+        }
+        public static void update(int id, Student stud)
+        {
+            int i;
+            foreach (Student s in st)
+            {
+                if (s.id == id)
+                {
+                    i = st.IndexOf(s);
+                    st[i].nume = stud.nume;
+                    st[i].prenume = stud.prenume;
+                    st[i].an = stud.an;
+                    st[i].mediegenerala = stud.mediegenerala;
+                    st[i].facultate = stud.facultate;
+                }
+
+            }
+        }
+    }
+}
